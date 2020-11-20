bbd7c14d14ca776dd4db6d5aa1c31fb21319ca7d
FanosLab <53920867+FanosLab@users.noreply.github.com>
2020-11-19 17:20:06 -0800

    Add files via upload

---
+++ SLLSet.java
@@ -0,0 +1,70 @@
+
+public class SLLSet {
+    private int size;
+    private SLLNode head;
+
+    public SLLSet() {
+        
+    }
+
+    public SLLSet(int[] sortedArray) {
+        
+    }
+
+    public int getSize() {
+		
+        return -1;
+    }
+
+    public SLLSet copy() {
+        
+
+        return new SLLSet();
+    }
+
+    public boolean isIn(int v) {
+        
+
+        return false;
+    }
+
+    public void add(int v) {
+        
+    }
+
+    public void remove(int v) {
+        
+    }
+
+    public SLLSet union(SLLSet s) {
+        
+
+        return s;
+    }
+
+    public SLLSet intersection(SLLSet s) {
+        
+
+        return s;
+    }
+
+    public SLLSet difference(SLLSet s) {
+        
+
+        return s;
+    }
+
+    public static SLLSet union(SLLSet[] sArray) {
+        SLLSet ss = new SLLSet();
+
+        
+
+        return ss;
+    }
+
+    @Override
+    public String toString() {
+		
+        return "";
+    }
+}
